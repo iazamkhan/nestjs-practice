670326c97d5a8c7e60a4cfbe38266f2a9dd846a5
Azam Khan <azamkhan561@gmail.com>
2026-06-11 09:54:09 +0530

    Understanding pipes for validating requests

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,10 @@ import {
   Param,
   Query,
   Body,
+  Headers,
+  Ip,
+  ParseIntPipe,
+  DefaultValuePipe,
 } from '@nestjs/common';
 
 //http://localhost:3000
@@ -15,16 +19,28 @@ import {
 @Controller('users')
 export class UsersController {
   @Get('/:id/')
-  public getUsers(@Param('id') id: any, @Query() query: any) {
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', ParseIntPipe, new DefaultValuePipe(10)) limit: number,
+    @Query('page', ParseIntPipe, new DefaultValuePipe(1)) page: number,
+  ) {
     console.log(id);
-    console.log(query);
+    console.log(typeof id);
+    console.log(limit, typeof limit);
+    console.log(page, typeof page);
 
     return 'You sent a get request to users endpoint';
   }
 
   @Post()
-  public createUsers(@Body() body: any) {
+  public createUsers(
+    @Body() body: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
     console.log(body);
+    console.log(headers);
+    console.log(ip);
     return 'You sent a post request to users endpoint';
   }
 }
