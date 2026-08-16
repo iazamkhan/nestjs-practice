b54fc248335082d8d119a3d42c782875ce572d82
Azam Khan <azamkhan561@gmail.com>
2026-08-16 13:53:22 +0530

    Create user entity and repository

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostDto } from './dtos/create-post.dto';
+import { PatchPostDto } from './dtos/patch-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
@@ -17,8 +18,28 @@ export class PostsController {
     return this.postsService.findAll(userId);
   }
 
+  @ApiOperation({
+    summary: 'Creates a new blog post',
+  })
+  @ApiResponse({
+    status: 201,
+    description:
+      'You get a 201 response in case your post has been created successfully',
+  })
   @Post()
   public createPost(@Body() createPostDTO: CreatePostDto) {
     console.log(createPostDTO);
+  }
+
+  @ApiOperation({
+    summary: 'Updates an existing blog post',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '200 response if a blog post is updated successfully',
+  })
+  @Patch()
+  public updatePost(@Body() patchPostDto: PatchPostDto) {
+    console.log(patchPostDto);
   }
 }
--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,22 +1,45 @@
 import { Injectable, Inject, forwardRef } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
+import { Repository } from 'typeorm';
+import { User } from '../user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from '../dtos/create-user.dto';
 
+/**
+ * Class to connect to Users table and connect to business operations
+ */
 @Injectable()
 export class UsersService {
   constructor(
-    //Injecting auth service
-    @Inject(forwardRef(() => AuthService))
-    private readonly authService: AuthService,
+    /**
+     * Injecting Users Repository
+     */
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
   ) {}
+
+  public async createUser(createUserDTO: CreateUserDto) {
+    //Check if the user exists with the same email
+    const existingUser = await this.usersRepository.findOne({
+      where: { email: createUserDTO.email },
+    });
+    //Handle exception
+    //Create a new user
+    let newUser = this.usersRepository.create(createUserDTO);
+    newUser = await this.usersRepository.save(newUser);
+    return newUser;
+  }
+
+  /**
+   * The method to get all the users from the database
+   */
 
   public findAll(
     getUserParamDto: GetUsersParamDto,
     limit: number,
     page: number,
   ) {
-    const isAuth = this.authService.isAuth();
-    console.log(isAuth);
     return [
       {
         firstName: 'John',
@@ -28,7 +51,9 @@ export class UsersService {
       },
     ];
   }
-  //Find one by id
+  /**
+   * Find a single user by the id of a user
+   */
   public findOneById(id: string) {
     return {
       firstName: 'John',
