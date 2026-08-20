93f4d236c11ca8bd7cac6966fd1ef98b929fca76
Azam Khan <azamkhan561@gmail.com>
2026-08-20 17:29:09 +0530

    Creating meta options

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -104,27 +104,21 @@ export class CreatePostDto {
   tags?: string[];
 
   @ApiPropertyOptional({
-    type: 'array',
+    type: 'object',
     required: false,
     items: {
       type: 'object',
       properties: {
-        key: {
-          type: 'string',
-          description: 'The key can be any identifier for your meta option',
-          example: 'sidebarEnabled',
-        },
-        value: {
-          type: 'any',
-          description: 'Any value for the key',
-          example: true,
+        metavalue: {
+          type: 'json',
+          description: 'The metaValue is a JSON string',
+          example: '{"sidebarEnabled": true}',
         },
       },
     },
   })
   @IsOptional()
-  @IsArray()
   @ValidateNested({ each: true })
   @Type(() => CreatePostMetaOptionsDto)
-  metaOptions?: CreatePostMetaOptionsDto[];
+  metaOptions?: CreatePostMetaOptionsDto | null;
 }
--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,12 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/providers/users.service';
+import { CreatePostDto } from '../dtos/create-post.dto';
+import { Repository } from 'typeorm';
+import { Post } from '../post.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MetaOption } from 'src/meta-options/meta-option.entity';
 
 @Injectable()
 export class PostsService {
   constructor(
     /*Injecting UsersService*/
     public readonly usersService: UsersService,
+    /**
+     * Inject postRepository
+     */
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+
+    /**
+     * Inject metaOptionsRepository
+     */
+    @InjectRepository(MetaOption)
+    private readonly metaOptionsRepository: Repository<MetaOption>,
   ) {}
+
+  /**
+   * Creating new posts
+   */
+  public create(@Body() createPostDto: CreatePostDto) {
+    //Create metaOptions
+    let metaOptions = createPostDto.metaOptions
+      ? this.metaOptionsRepository.create(createPostDto.metaOptions)
+      : null;
+    //Create post
+    //Add metaoptions to the post
+    //Return the post
+  }
+
   public findAll(userId: string) {
     const user = this.usersService.findOneById(userId);
     return [
