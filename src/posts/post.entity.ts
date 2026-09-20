24d1be4ed8c6150f15a221f277663726a12fba0b
Azam Khan <azamkhan561@gmail.com>
2026-09-20 06:59:55 +0530

    Added tags and posts delete endpoints

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -64,7 +64,7 @@ export class Post {
   @ManyToOne(() => User, (user) => user.posts, { eager: true })
   author: User;
 
-  @ManyToMany(() => Tag, { eager: true })
+  @ManyToMany(() => Tag, (tag) => tag.posts, { eager: true })
   @JoinTable()
   tags?: Tag[];
 }
